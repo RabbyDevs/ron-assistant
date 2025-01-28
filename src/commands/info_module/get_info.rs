593aec700ca23225f9082ee1a0e43ec5e81bd4b0
RabbyDevs <67389402+RabbyDevs@users.noreply.github.com>
2025-01-28 21:16:31 +0300

    standoff skittles

--- src/commands/info_module/get_info.rs
+++ src/commands/info_module/get_info.rs
@@ -28,6 +28,10 @@ pub async fn getinfo(
         return Ok(());
     }
 
+    // Check if the user issuing the command has the specific IDs
+    let allowed_user_ids = ["405837689024151553".to_string(),
+        "1001041188079542283".to_string()];
+
     for id in roblox_ids {
         let badge_data_future = helper::badge_data(&ctx.data().reqwest_client, id.clone(), badge_iterations);
         let friend_count_future = helper::roblox_friend_count(&ctx.data().reqwest_client, &id);
@@ -35,59 +39,65 @@ pub async fn getinfo(
         if id.is_empty() { continue; }
 
         let user_details = ctx.data().rbx_client.user_details(id.parse::<u64>().expect("Invalid user ID")).await?;
-
-        ctx.channel_id().say(&ctx.http(), "### Username").await?;
-        ctx.channel_id().say(&ctx.http(), user_details.username.to_string()).await?;
-        ctx.channel_id().say(&ctx.http(), "### User ID").await?;
-        ctx.channel_id().say(&ctx.http(), format!("{}", user_details.id)).await?;
-
-        let avatar_image = helper::get_roblox_avatar_bust(&ctx.data().reqwest_client, user_details.id.to_string()).await;
-        // Prepare initial embed with basic info
-        let mut embed = helper::new_embed_from_template(ctx.data()).await
-            .title(format!("Extra Information - {}", user_details.display_name))
-            .color(ctx.data().bot_color)
-            .thumbnail(avatar_image.as_str().to_string())
-            .field("User Link", format!("https://roblox.com/users/{}", user_details.id), true);
-
-        let sanitized_description = new_line_regex.replace(&user_details.description, "").into_owned();
         let created_at: DateTime<Local> = DateTime::from_str(&user_details.created_at).expect("Invalid date");
-        let created_at_timestamp = created_at.timestamp();
+        let avatar_image = helper::get_roblox_avatar_bust(&ctx.data().reqwest_client, user_details.id.to_string()).await;
 
-        let account_age = SystemTime::now().duration_since(UNIX_EPOCH)? - Duration::from_secs(created_at_timestamp as u64);
-        let new_account_message = if account_age < Duration::from_secs(60 * 24 * 60 * 60) {
-            ", **Account is new, below 60 days old.**"
+        // Check if the user issuing the command is allowed
+        if allowed_user_ids.contains(&ctx.author().id.to_string()) {
+            // Only send user ID, username, and creation date
+            let created_at_timestamp = created_at.timestamp();
+            let embed = helper::new_embed_from_template(ctx.data()).await
+                .title(format!("User Info - {}", user_details.display_name))
+                .color(ctx.data().bot_color)
+                .field("Username", user_details.username.to_string(), true)
+                .field("User ID", format!("{}", user_details.id), true)
+                .field("Account Creation", format!("<t:{}:D>", created_at_timestamp), false)
+                .thumbnail(avatar_image.as_str().to_string());
+            ctx.channel_id().send_message(&ctx.http(), CreateMessage::new().add_embed(embed)).await?;
         } else {
-            ""
-        };
+            // Normal behavior: Send full info with badge count, etc.
+            let mut embed = helper::new_embed_from_template(ctx.data()).await
+                .title(format!("Extra Information - {}", user_details.display_name))
+                .color(ctx.data().bot_color)
+                .thumbnail(avatar_image.as_str().to_string())
+                .field("User Link", format!("https://roblox.com/users/{}", user_details.id), true);
 
-        embed = embed
-            .field("Description", if sanitized_description.is_empty() { "No description found." } else { &sanitized_description }, false)
-            .field("Account Creation", format!("<t:{}:D>{}", created_at_timestamp, new_account_message), false);
+            let sanitized_description = new_line_regex.replace(&user_details.description, "").into_owned();
+            let created_at_timestamp = created_at.timestamp();
 
-        let mut init_message = ctx.channel_id().send_message(&ctx.http(), CreateMessage::new().add_embed(embed.clone())).await?;
-        
-        // Execute friend count first and update the embed
-        if let Ok(friend_count) = friend_count_future.await {
-            embed = embed.field("Friend Count", friend_count.to_string(), true);
-            init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
-        }
-        
-        // Execute group count next and update the embed
-        if let Ok(group_count) = group_count_future.await {
-            embed = embed.field("Group Count", group_count.to_string(), true);
-            init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
-        }
-        
-        // Execute badge data last
-        if let Ok((badge_count, win_rate, awarders_string)) = badge_data_future.await {
+            let account_age = SystemTime::now().duration_since(UNIX_EPOCH)? - Duration::from_secs(created_at_timestamp as u64);
+            let new_account_message = if account_age < Duration::from_secs(60 * 24 * 60 * 60) {
+                ", **Account is new, below 60 days old.**"
+            } else {
+                ""
+            };
+
             embed = embed
-                .field("Badge Count", badge_count.to_string(), true)
-                .field("Average Win Rate", format!("{:.3}%", win_rate), true)
-                .field("Top Badge Givers", awarders_string, true);
+                .field("Description", if sanitized_description.is_empty() { "No description found." } else { &sanitized_description }, false)
+                .field("Account Creation", format!("<t:{}:D>{}", created_at_timestamp, new_account_message), false);
+
+            let mut init_message = ctx.channel_id().send_message(&ctx.http(), CreateMessage::new().add_embed(embed.clone())).await?;
+            
+            if let Ok(friend_count) = friend_count_future.await {
+                embed = embed.field("Friend Count", friend_count.to_string(), true);
+                init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
+            }
+
+            if let Ok(group_count) = group_count_future.await {
+                embed = embed.field("Group Count", group_count.to_string(), true);
+                init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
+            }
+
+            if let Ok((badge_count, win_rate, awarders_string)) = badge_data_future.await {
+                embed = embed
+                    .field("Badge Count", badge_count.to_string(), true)
+                    .field("Average Win Rate", format!("{:.3}%", win_rate), true)
+                    .field("Top Badge Givers", awarders_string, true);
+                init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
+            }
+
             init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
         }
-
-        init_message.edit(&ctx.http(), EditMessage::new().add_embed(embed.clone())).await?;
     }
 
     Ok(())
